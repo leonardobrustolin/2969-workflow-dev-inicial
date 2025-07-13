efca455640e7f890005df7b3930b31055935921b
Leonardo Brustolin <leobrustolin1@gmail.com>
2025-07-13 09:19:08 -0300

    feat: add: modelo eventos e teste do modelo

---
+++ src/test/models/evento.test.js
@@ -0,0 +1,21 @@
+import {
+  describe, expect, it, jest,
+} from '@jest/globals';
+import Evento from '../../models/evento.js';
+
+describe('Testando o modelo Evento', () => {
+  const objetoEvento = {
+    nome: 'Evento test',
+    descricao: 'Teste de evento',
+    data: '2025-01-01',
+    autor_id: '1',
+  };
+
+  it('Deve instanciar uma novo evento', () => {
+    const evento = new Evento(objetoEvento);
+
+    expect(evento).toEqual(
+      expect.objectContaining(objetoEvento),
+    );
+  });
+});
